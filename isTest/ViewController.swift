b41e226933551969994d01943eae8d90306aef86
Roman Sumorokov <iromka74@gmail.com>
2021-09-02 20:03:50 +0500

    Initial Commit

---
+++ isTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  isTest
+//
+//  Created by Рома Сумороков on 02.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
